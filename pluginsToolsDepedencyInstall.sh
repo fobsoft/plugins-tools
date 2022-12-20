4c954993114b156a52bf7361012c02299a8bb470
fobsoft <34339628+fobsoft@users.noreply.github.com>
2022-12-20 04:12:24 -0500

    no message

---
+++ pluginsToolsDepedencyInstall.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/ajax/pluginsToolsDepedency.ajax.php -O ../../../core/ajax/pluginsToolsDepedency.ajax.php
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/class/pluginsToolsDepedency.class.php -O ../../../core/class/pluginsToolsDepedency.class.php
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/js/pluginToolsDepedency.js -O ../../../core/js/pluginToolsDepedency.js
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/desktop/modal/pluginsToolsDepedencyLog.execution.php -O ../../../desktop/modal/pluginsToolsDepedencyLog.execution.php
