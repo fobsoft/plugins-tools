cc3336ead81e0e16f3095f3a956d9d2dda00daf7
fobsoft <34339628+fobsoft@users.noreply.github.com>
2023-01-29 08:54:26 -0500

    no message

--- pluginsToolsDepedencyInstall.sh
+++ pluginsToolsDepedencyInstall.sh
@@ -3,4 +3,6 @@
 wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/ajax/pluginsToolsDepedency.ajax.php -O ../../../core/ajax/pluginsToolsDepedency.ajax.php
 wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/class/pluginsToolsDepedency.class.php -O ../../../core/class/pluginsToolsDepedency.class.php
 wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/core/js/pluginToolsDepedency.js -O ../../../core/js/pluginToolsDepedency.js
-wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/desktop/modal/pluginsToolsDepedencyLog.execution.php -O ../../../desktop/modal/pluginsToolsDepedencyLog.execution.php
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/desktop/common/js/pluginToolsDepedency.js -O ../../../desktop/common/js/pluginToolsDepedency.js
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/desktop/modal/pluginsToolsDepedency.showLog.php -O ../../../desktop/modal/pluginsToolsDepedency.showLog.php
+wget https://raw.githubusercontent.com/fobsoft/plugins-tools/Beta/desktop/modal/pluginsToolsDepedency.jsonEdit.EqLogic.php -O ../../../desktop/modal/pluginsToolsDepedency.jsonEdit.EqLogic.php
